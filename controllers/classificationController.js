5b343ee897446d6950add65c06f4f21aa6b007f3
Thaofoo <mthafixh@gmail.com>
2024-12-04 12:57:36 +0700

    feat: inference api returns url

--- controllers/classificationController.js
+++ controllers/classificationController.js
@@ -1,12 +1,25 @@
 const { inference } = require("../modules/classification-model");
 const { preprocessImage } = require("../modules/image-preprocessing");
+require("dotenv").config()
 
   // Remove article from favorites
 const infer = async (req, res) => {
+  try {
     const preprocessedImage = await preprocessImage(req.file.buffer);
-    const prediction = await inference(preprocessedImage);
-    res.json({ classification: prediction});
+    const [index, label] = await inference(preprocessedImage);
+    res.json(
+      { 
+        classification: label, 
+        index:index, 
+        url:process.env.BASE_URL + "/herbs/" + index
+      }
+    );
+  } catch (err) {
+    res
+        .status(500)
+        .json({ message: "Inference error", error: err.message });
   };
+}
 
 module.exports = { infer };
   
