3c29187b1eff01f40121b9024b6eb4d43db2b238
Thaofoo <mthafixh@gmail.com>
2024-12-04 15:40:34 +0700

    fix: multer

--- routes/classificationRoutes.js
+++ routes/classificationRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const classificationController = require("../controllers/classificationController");
 const router = express.Router();
-const multer = require('multer');
-const upload = multer({ storage: multer.memoryStorage() });
+const Multer = require('multer');
+const multer = Multer({
+    storage: Multer.MemoryStorage,
+    fileSize: 3 * 1024 * 1024,
+    limits: {
+        fileSize: 3 * 1024 * 1024
+    }
+})
 require("body-parser");
 
-router.post("/infer", upload.single('img'), classificationController.infer);
+router.post("/infer", multer.single('image'), classificationController.infer);
 
 module.exports = router;
